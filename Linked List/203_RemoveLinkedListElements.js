ac7432dd17e352f0eab9c2a1d920c7ae09a930cd
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-12-09 21:18:06 -0800

    remove linked list

---
+++ Linked List/203_RemoveLinkedListElements.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  // Edge case: when the first element is the value
+  while (head && head.val === val) {
+    head = head.next;
+  }
+
+  if (!head) return null;
+
+  // Traverse through the list
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.next.val === val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
